5151ceff33c1123557ee460ab2636fd9181c8f33
Raphael Charousset <raphael.charousset@gmail.com>
2022-01-04 17:25:50 +0100

    basic of aboutme

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -3,8 +3,8 @@ import './aboutme.scss'
 export const AboutMe = () => {
   return (
     <section className='about'>
-      <p>Je suis actuellement à la recherche d'une entreprise avec laquelle je pourrais metter en pratique mes compétences, continuer de grandir et partager mon dynamisme</p>
-      <p>J'aime concevoir le développeur web comme le technicien qui permet de relier l'humain dans le monde d'aujourd'hui et celui de demain</p>
+      <p className='about__goal'>Je suis actuellement à la recherche d'une entreprise avec laquelle je pourrais metter en pratique mes compétences, continuer de grandir et partager mon dynamisme.</p>
+      <p className='about__conviction'>J'aime concevoir le développeur web comme le technicien qui permet de relier l'humain dans le monde d'aujourd'hui et celui de demain.</p>
     </section>
   )
 }
