6af6eb5781bb0bf73ece2f73864a3f1870137b9b
Raphael Charousset <raphael.charousset@gmail.com>
2022-01-11 15:40:38 +0100

    adding buttons in desktop

--- src/components/ProjectsHome/ProjectsHome.tsx
+++ src/components/ProjectsHome/ProjectsHome.tsx
@@ -1,30 +1,64 @@
-import { useState } from 'react'
 import { NavLink } from 'react-router-dom';
 import './projectshome.scss'
 import { projects } from './../../data/index';
 import { SingleProject } from './SingleProject/SingleProject';
+import { createRef, MouseEvent, useState, useEffect } from 'react';
 
 export const ProjectsHome = () => {
-  const [count, setCount] = useState(0)
-  projects.sort((a, b) => b.year - a.year)
+  console.log('render bro');
+  
+  useEffect(() => {
+    console.log('render');
+    projects.sort((a, b) => b.year - a.year)
+  }, [])
+  
+  const gap = 36
+  const ref = createRef()
+  const [position, setPosition] = useState(0)
+  const [selected, setSelected] = useState(0)
+  
+  const handleClick: (e: MouseEvent<HTMLButtonElement>) => void = (e) => {
+    const width: number = ref.current?.offsetWidth
+    const index: number = e.target.value
+    setPosition(index * (width + gap))
+    setSelected(index)
+  }
 
   return (
     <section className='projects'>
       <h2 className='projects__title'>Mes réalisations</h2>
 
       <div className="projects__container">
-        <div className="projects__container__slider">
+        <div
+          className="projects__container__slider"
+          style={{ 
+            transform: `translateX(-${position}px)`,
+            gap: `${gap}px`
+          }}
+        >
           {projects.map((project, index) => 
-              <SingleProject key={index} {...project} />
+              <SingleProject
+                key={index} 
+                {...project}
+                ref={index === 0 ? ref : null}
+              />
           )}
         </div>
       </div>
 
       <div className="projects__buttons">
-        {/* buttons for each unshown projects ? */}
-        {/* onclick, translateX */}
+        {projects.map((_, index) => (
+          index < (projects.length - 1) &&
+          <button
+            value={index}
+            key={index}
+            onClick={handleClick}
+            className={index == selected ? 'selected' : 'unselected'}
+          />
+        ))}
       </div>
-      <NavLink to="/projects">tous les projets</NavLink>
+      
+      {/* <NavLink to="/projects">tous les projets</NavLink> */}
     </section>
   )
 }
--- src/components/ProjectsHome/SingleProject/SingleProject.tsx
+++ src/components/ProjectsHome/SingleProject/SingleProject.tsx
@@ -1,43 +1,43 @@
+import { forwardRef } from 'react';
 import Github from '../../../assets/websiteIcons/Github';
 import Web from '../../../assets/websiteIcons/Web';
 import { Project } from './../../../interfaces/Project';
 import './singleProject.scss'
 
-export const SingleProject = (
-  {name, year, coverImg, coverImgAlt, stack, ghLink, urlLink, description}: Project
-) => {
-  return (
-    <article className="single-project">
-      <header className="single-project__header">
-        <div className="single-project__header__img">
-          <img src={coverImg} alt={coverImgAlt} />
-        </div>
-        <div className="single-project__header__stack">
-          {stack.map((item, index) => <span key={index}>{item}</span>)}
-        </div>
-      </header>
-
-      <div className="single-project__main">
-        <section className="single-project__container">
-          <h3 className="single-project__container__title">
-            <strong>{name}</strong> - <span>{year}</span>
-          </h3>
-          <p>{description}</p>
-        </section>
-
-        <footer className='single-project__footer'>
-          {ghLink && (
-            <a href={ghLink} target='blanck'>
-              <Github />
-            </a>
-          )}
-          {urlLink && (
-            <a href={urlLink} target='blanck'>
-              <Web />
-            </a>
-          )}
-        </footer>
+export const SingleProject = forwardRef((
+  {name, year, coverImg, coverImgAlt, stack, ghLink, urlLink, description}: Project,
+  ref
+) => (
+  <article className="single-project" ref={ref}>
+    <header className="single-project__header">
+      <div className="single-project__header__img">
+        <img src={coverImg} alt={coverImgAlt} />
       </div>
-    </article>
-  )
-}
+      <div className="single-project__header__stack">
+        {stack.map((item, index) => <span key={index}>{item}</span>)}
+      </div>
+    </header>
+
+    <div className="single-project__main">
+      <section className="single-project__container">
+        <h3 className="single-project__container__title">
+          <strong>{name}</strong> - <span>{year}</span>
+        </h3>
+        <p>{description}</p>
+      </section>
+
+      <footer className='single-project__footer'>
+        {ghLink && (
+          <a href={ghLink} target='blanck'>
+            <Github />
+          </a>
+        )}
+        {urlLink && (
+          <a href={urlLink} target='blanck'>
+            <Web />
+          </a>
+        )}
+      </footer>
+    </div>
+  </article>
+))
