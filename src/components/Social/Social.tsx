0b6ebd3a83b70416e35c29fdf5f91071ebb420fd
Raphael Charousset <raphael.charousset@gmail.com>
2022-01-12 11:20:29 +0100

    socials done with basic styling

--- src/components/Social/Social.tsx
+++ src/components/Social/Social.tsx
@@ -1,19 +1,53 @@
-import { Link } from 'react-router-dom'
+import { CodingameLogo } from '../../assets/socialLogo/CodingameLogo'
+import { GithubLogo } from '../../assets/socialLogo/GithubLogo'
+import { LinkedinLogo } from '../../assets/socialLogo/LinkedinLogo'
+import { MailLogo } from '../../assets/socialLogo/MailLogo'
+import { PhoneLogo } from '../../assets/socialLogo/PhoneLogo'
 import './social.scss'
+import SocialItem from './SocialItem/SocialItem'
 
 export const Social = () => {
   return (
     <section className='social'>
       <h2 className='social__title'>Gardons le contact !</h2>
-      <div>Phone</div>
-      <div>Mail</div>
-      <div>Linkedin</div>
-      <div>Github</div>
-      <div>
-        <a href='https://www.codingame.com/profile/924d621de97f78b756a0e1cefdc5f1e76161924' target='blanck'>
-          Un clash of Code ?
-        </a>
+
+      <div className="social__contacts">
+      <SocialItem
+          social='Téléphone'
+          href='tel:0676896496'
+          content='06.76.98.64.96'
+          logo={<PhoneLogo />}
+        />
+
+        <SocialItem
+          social='Email'
+          href='mailto:raphael.charousset@gmail.com'
+          content='raphael.charousset@gmail.com'
+          logo={<MailLogo />}
+        />
       </div>
+
+      <div className="social__websites">
+        <SocialItem
+          social='LinkedIn'
+          href='https://www.linkedin.com/in/raphaelcharousset/'
+          content='linkedin.com/in/raphaelcharousset'
+          logo={<LinkedinLogo />}
+        />
+        <SocialItem
+          social='Github'
+          href='https://github.com/RaphaelCharousset'
+          content='github.com/RaphaelCharousset'
+          logo={<GithubLogo />}
+        />
+        <SocialItem
+          social='CodinGame'
+          href='https://www.codingame.com/profile/924d621de97f78b756a0e1cefdc5f1e76161924'
+          content='Un Clash of Code ?'
+          logo={<CodingameLogo />}
+        />
+      </div>
+
     </section>
   )
 }
