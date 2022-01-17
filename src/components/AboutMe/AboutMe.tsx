abbb3f07f1dd36164fb79b8c596fd4d66bba3590
Raphael Charousset <raphael.charousset@gmail.com>
2022-01-17 11:06:56 +0100

    changing about me section but can't find a nice style

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,21 +1,26 @@
 import './aboutme.scss'
 
 export const AboutMe = () => {
+  const opquastLink = 'https://directory.opquast.com/fr/certificat/G68027/'
+
   return (
     <section className='about'>
-      <p className="about__testament">
-        Je suis un 
-        Je suis diplômé du titre professionnel Développeur Web et Web Mobile suite à une formation à l'école O'Clock. 
-      </p>
+      <h2 className='about__title'>À propos</h2>
 
-      <p className='about__conviction'>
-        J'aime concevoir le développeur web comme le technicien qui permet d'accéder facilement à des informations. Construire des sites internet, c'est relier les humains entre eux.
-      </p>
+      <div className="about__container">
+        <p className="about__container__testament">
+          Vous êtes à la recherche de quelqu'un prêt à relever des défis ? <br /> Quelqu'un d'autonome mais pour qui le travail d'équipe est essentiel ? <br /> Quelqu'un qui code pour délivrer une expérience utilisateur de qualité ? <br /> Vous êtes tombé sur la bonne personne ! <br />
+          Je suis diplômé du titre professionnel Développeur Web et Web Mobile depuis 2021 et d'une <a href={opquastLink} target='_blank'>certification Opquast</a> en Maitrise de la qualité en projet web (niveau Avancé). 
+        </p>
 
+        <p className='about__container__conviction'>
+          J'aime concevoir le développeur web comme le technicien qui permet d'accéder facilement à des informations. Construire des sites internet, c'est relier les humains entre eux.
+        </p>
 
-      <p className='about__goal'>
-        Je suis actuellement à la recherche d'une entreprise avec laquelle je pourrais mettre en pratique mes compétences, continuer de grandir et partager mon dynamisme.
-      </p>
+        <p className='about__container__goal'>
+          Je cherche à mettre en pratique mes compétences, continuer de grandir et partager mon dynamisme au quotidien.
+        </p>
+      </div>
 
     </section>
   )
